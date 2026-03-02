58c4debe8f182ea3267e39bc306c9bb112786af9
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-03-02 23:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function rot13(str) {
-    let res = "";
-    
-    for (let i = 0; i < str.length; i++) {
-        let ch = str[i];
-        
-        if (ch >= 'A' && ch <= 'Z') {
-            let code = ch.charCodeAt(0) - 65; // 0-25
-            code = (code + 13) % 26;
-            res += String.fromCharCode(code + 65);
-        } else {
-            res += ch; // keep punctuation/space
-        }
+function isDate(input) {
+    // valid Date object
+    if (input instanceof Date) {
+        return !isNaN(input.getTime());
     }
-    
-    return res;
-}
 
-// Driver code (AccioJob style)
-let input = require("fs").readFileSync(0, "utf-8").trim();
-console.log(rot13(input));
+    // try parsing other types (string, number)
+    const d = new Date(input);
+    return !isNaN(d.getTime());
+}
